187c429871c717632714f45119fd4f1c12cad3a2
Laura Vingada <laura@vingada.com>
2017-06-13 19:42:12 -0400

    added leds to botbot

--- fineBotbot.js
+++ fineBotbot.js
@@ -5,15 +5,22 @@ var keypress = require('keypress');
 var board = new five.Board();
 
 board.on('ready', function() {
+
+  // for the led
+
+  var led = new five.Led(13);
+
+  // all this running the left and right servos and the keyboard controls
+
   console.log('Botbot loves you!');
   console.log('Control Botbot with arrow keys and space to stop.');
 
   var leftWheel = new five.Servo({
-    pin: 10,
+    pin: 9,
     type: 'continuous'
   }).stop();
   var rightWheel = new five.Servo({
-    pin: 9,
+    pin: 10,
     type: 'continuous'
   }).stop();
 
@@ -31,6 +38,7 @@ board.on('ready', function() {
       console.log('Forward, ho');
       leftWheel.ccw();
       rightWheel.cw();
+      led.on();
     } else if (key.name === 'down') {
       console.log('Backwards we go!');
       leftWheel.cw();
@@ -39,14 +47,17 @@ board.on('ready', function() {
       console.log('Starboard!');
       leftWheel.ccw();
       rightWheel.ccw();
+      led.blink(500);
     } else if (key.name === 'right') {
       console.log('Port!');
       leftWheel.cw();
       rightWheel.cw();
+      led.blink(1000);
     } else if (key.name === 'space') {
       console.log('Stopping!');
       leftWheel.stop();
       rightWheel.stop();
+      led.stop();
     }
   });
 });
--- litBotbot.js
+++ litBotbot.js
@@ -5,15 +5,23 @@ var keypress = require('keypress');
 var board = new five.Board();
 
 board.on('ready', function() {
+
+  // for the led
+
+  var led = new five.Led(13);
+  led.blink(1000);
+
+  // all this running the left and right servos and the keyboard controls
+
   console.log('Botbot loves you!');
   console.log('Control Botbot with arrow keys and space to stop.');
 
   var leftWheel = new five.Servo({
-    pin: 10,
+    pin: 9,
     type: 'continuous'
   }).stop();
   var rightWheel = new five.Servo({
-    pin: 9,
+    pin: 10,
     type: 'continuous'
   }).stop();
 
